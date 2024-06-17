7e2c8004c195d8d23fa22e6cd26605c788ef1ced
Ujwal Jagadeesh <59735375+yoU-Jay@users.noreply.github.com>
2024-06-17 22:12:30 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,22 +12,9 @@ PARAM2_VALUE=$(aws ssm get-parameter --name "$PARAM2_NAME" --with-decryption --q
 PARAM3_VALUE=$(aws ssm get-parameter --name "$PARAM3_NAME" --with-decryption --query "Parameter.Value" --output text)
 PARAM4_VALUE=$(aws ssm get-parameter --name "$PARAM4_NAME" --with-decryption --query "Parameter.Value" --output text)
 
-# Define the variables to be replaced in gcal.py
-VAR1="DEFAULT_CALENDAR_ID"
-VAR2="database_id"
-VAR3="NOTION_TOKEN"
-VAR4="urlRoot"
+export DEFAULT_CALENDAR_ID=$PARAM1_VALUE
+export DATABASE_ID=$PARAM2_VALUE
+export NOTION_TOKEN=$PARAM3_VALUE
+export URL_ROOT=$PARAM4_VALUE
 
-# Path to the Python file
-FILE_PATH="home/ec2-user/notion-sync/Notion-and-Google-Calendar-2-Way-Sync/Notion-GCal-2WaySync-Public.py"
-
-# Replace the variables in the Python file
-sed -i.bak "s|$VAR1 = .*|$VAR1 = '$PARAM1_VALUE'|g" "$FILE_PATH"
-sed -i.bak "s|$VAR2 = .*|$VAR2 = '$PARAM2_VALUE'|g" "$FILE_PATH"
-sed -i.bak "s|$VAR3 = .*|$VAR3 = '$PARAM3_VALUE'|g" "$FILE_PATH"
-sed -i.bak "s|$VAR4 = .*|$VAR4 = '$PARAM4_VALUE'|g" "$FILE_PATH"
-
-# Optional: Remove backup file created by sed
-rm "$FILE_PATH.bak"
-
-echo "Values replaced successfully in $FILE_PATH"
+echo "completed"
